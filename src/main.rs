7e5c3d57d4565ff8f88d0a6e79c07941ef163d41
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-30 14:44:18 +0530

    References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-fn main() { // "s1", "s2" & "s3" invalid here
-    let s1 = gets_ownership(); // "s1" gets ownership from function
-    let s2 = String::from("hello"); // "s2" comes into scope and stored on heap
-    let s3 = gives_and_takes_back_ownership(s2); // "s2" `move` to function and invalidated
-                                                 // "s3" gets ownership from function
+fn main() {
+    let s1 = String::from("hello");
 
-    println!("s1 is {}", s1); // "s1" is valid here
-    // println!("s2 is {}", s2); // Throws compile time error
-    println!("s3 is {}", s3); // "s3" is valid here
+    let length = calculate_length(&s1); // &s1 creates a reference
+                                        // reference `immutable` like variables
 
-} // "s1" & "s2" goes out of scope and `drop` called
+    println!("Length of {} is {}", s1, length); // "s1" valid here
 
-fn gets_ownership() -> String { // "s" is invalid here
-    let s = String::from("hello");
+    change(&s1);
+} // "s1" goes out of scope and is dropped
 
-    s // "s" `move` to calling function
-}
+fn calculate_length(s: &String) -> usize { // "s" reference to String
+                                           // "s" `borrow` from "s1"
+                                           // "s" doesn't own
+    s.len()
+} // "s" goes out of scope and nothing is dropped
 
-fn gives_and_takes_back_ownership(s: String) -> String { // "s" comes into scope and takes ownership
-    s // "s" `move` to calling function
+fn change(s: &String) { 
+    // s.push_str(", world"); // Compile time error trying to modify immutable reference
 }
