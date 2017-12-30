6cdf66d830c82b57716cf34904b8e7a7ba89223b
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-30 13:32:28 +0530

    Ways Variables and Data interact: Move

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-fn main() { // "s1" not valid here
-    let s1 = "hello"; // "s1" comes into scope
-                      // "s1" is of know size and so allocated in stack
-                      // "s1" can't be mutated
-    
-    println!("s1 is {}", s1); // "s1" valid here
+fn main() { // "x", "y", "s1" & "s2" invalid here
+    let x = 5; // "x" comes into scope
+    let y = x; // copy "x" to "y"
 
-    // "s2" not valid here
+    println!("x is {}, y is {}", x, y); // "x" is still valid here
+                                        // "x" is on stack
+                                        // integer types have `Copy` trait
 
-    let mut s2 = String::from("hello"); // "s2" comes into scope
-                                        // "s2" is of unknown size and stored in heap
-                                        // "s2" can be mutated
+    let s1 = String::from("hello"); // "s1" comes into scope & stored on heap
+    let s2 = s1; // "s1" `move` into "s2" and is invalidated
+                 // "s1" invalid here
 
-    s2.push_str(", world"); // "s2" valid here
-
+    //println!("s1 is {}", s1); // Throws compile time error
     println!("s2 is {}", s2); // "s2" valid here
-} // "s1" goes out of scope and no `drop` is called
+} // "x" and "y" goes out of scope and no `drop` is called
   // "s2" goes out of scope and `drop` is called
