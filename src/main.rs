e129a09715d80aababea59e9878dec5062aa98b6
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-31 00:25:23 +0530

    String Slices as Parameters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let s = "hello, world"; // String literal directly stored in binary
-                            // "&str" points to specific point in binary
-                            // "s" -> "&str" (immutable slice)
-                            // So, String literals are immutable by default
+    let s = String::from("hello world");
+    let literal = "hello world";
+
+    let word = first_word(&s);
+    let word = first_word(&s[..5]);
+    // String literals are *slices* too
+    let word = first_word(literal);
+    let word = first_word(&literal[..5]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &byte) in bytes.iter().enumerate() {
+        if byte == b' ' {
+            return &s[..i]
+        }
+    }
+
+    &s[..]
 }
