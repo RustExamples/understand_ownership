bd40f857e07330ddbccb41fd029c6fea2bfb3eb7
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-30 13:49:38 +0530

    Ownership and Functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-fn main() { // "s1" and "s2" invalid here
-    let s1 = String::from("hello"); // "s1" comes into scope and is stored on heap
-    let s2 = s1.clone(); // "s1" is deep copied to "s2"
-                         // "s1" still valid here
+fn main() { // "s" & "x" invalid here
+    let s = String::from("hello"); // "s" comes into scope and stored on heap
 
-    println!("s1 is {}, s2 is {}", s1, s2); // "s1" and "s2" valid here
-}  // "s1" & "s2" goes out of scope and `drop` is called
+    takes_ownership(s); // "s" `move` into function and is invalidated
+
+    // println("s is {}", s); // Throws compile time error
+
+    let x = 5; // "x" comes into scope
+
+    makes_copy(x); // "x" copied to function
+
+    println!("x is {}", x); // "x" still valid here
+} // "x" goes out of scope and no `drop` called
+
+fn takes_ownership(s: String) { // "s" comes into scope and takes ownership
+    println!("s is {}", s); // "s" valid here
+} // "s" goes out of scope and `drop` is called
+
+fn makes_copy(x: i32) { // "x" comes into scope
+    println!("x is {}", x); // "x" valid here
+} // "x" goes out of scope and no `drop` called
