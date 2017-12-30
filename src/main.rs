a4cb03430d81253966cce0eeca3d85c3f7e3c199
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-30 19:42:28 +0530

    Dangling reference

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
 fn main() {
-    let mut s1 = String::from("hello"); // make "s1" mutable
-
-    change(&mut s1); // create "mutable" reference of "s1"
-
-    println!("s1 is {}", s1);
-
-    // compile time error 
-    // atmost one mutable reference to "s1" possible
-    // let r1 = &mut s1;
-    // let r2 = &mut s1;
-
-    {
-        let r1 = &mut s1;
-    } // "r1" goes out of scope and returns what it borrowed
-
-    let r2 = &mut s1; // valid usage here
-
-    // compile time error 
-    // mutiple immutable references possible
-    let r1 = &s1;
-    let r2 = &s1;
-    // mixing mutable and immutable references in same scope
-    let r3 = &mut s1;
-
-} // "s1" goes out of scope and is dropped
-
-fn change(s: &mut String) {  // accept a "mutable" reference
-     s.push_str(", world"); 
+	let dangling_reference = get_dangling_reference(); // dangling reference
 }
+
+fn get_dangling_reference() -> &String { // Returns reference to String
+	let s = String::from("hello"); // "s" comes into scope
+    
+    &s // reference to "s" returned
+} // "s" goes out of scope and dropped
